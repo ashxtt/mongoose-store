5ac26edde003ea2bb1823012f91fa140002f7a28
ashxtt <111319560+ashxtt@users.noreply.github.com>
2022-10-25 20:38:30 -0700

    Finally gomongoose connected

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
 const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+require("dotenv").config()
+
+
+//!Database Connect
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+//! Database Error/Success
+const db = mongoose.connection
+db.on("error", (err) => console.log(err.message + " mongo ain't workin"))
+db.on('connected', () => console.log("mongo connected"))
+db.on('disconnected', () => console.log('mongo disconnected'))
+
+
+//!listener
+const PORT = process.env.PORT
+app.listen(PORT, ()=> console.log('WE LIVIN'))
